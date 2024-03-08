69299c77b752038e8ff08044f5ce0c2113a4ad4c
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-08 23:01:14 +0530

    popular page finish with the item page

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero.jsx/Hero'
+import Popular from '../Components/Popular/Popular'
 
 const Shop = () => {
   return (
     <div>
-          <Hero />
+        <Hero />
+        <Popular />
     </div>
   )
 }
