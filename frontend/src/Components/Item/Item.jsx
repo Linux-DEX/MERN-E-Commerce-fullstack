024e44f70bb3f237b59406b3714764cbcc1ea9fa
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-08 22:48:28 +0530

    item page design, props and css added

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Item.css";
 
-const Item = () => {
+const Item = (props) => {
   return (
-    <div>Item</div>
-  )
-}
+    <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">{props.new_price}</div>
+        <div className="item-price-old">{props.old_price}</div>
+      </div>
+    </div>
+  );
+};
 
-export default Item
+export default Item;
---
+++ frontend/src/Components/Popular/Popular.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Popular.css";
+import data_product from "../Assets/data";
+
+const Popular = () => {
+    return (
+        <div className="popular">
+            <h1>POPULAR IN WOMEN</h1>
+            <hr />
+            
+        </div>
+    );
+};
+
+export default Popular;
