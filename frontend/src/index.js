b6b3f3faabe65f893177f9f5cc7a0794463b02ac
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-09 10:25:14 +0530

    created context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ShopContextProvider from './Context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ShopContextProvider>
     <App />
-  </React.StrictMode>
+  </ShopContextProvider>
 );
 
 reportWebVitals();
