59f4fb0d71116225cc5b0c954c4650eb40b7eba5
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-09 15:06:43 +0530

    adding funtionality to Addtocart button

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
   const { product } = props;
+  const { addToCart } = useContext(ShopContext);
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
@@ -37,7 +39,9 @@ const ProductDisplay = (props) => {
           </div>
         </div>
         <div className="productdisplay-right-description">
-          A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershit or outer garment.
+          A lightweight, usually knitted, pullover shirt, close-fitting and with
+          a round neckline and short sleeves, worn as an undershit or outer
+          garment.
         </div>
         <div className="productdisplay-right-size">
           <h1>Select Size</h1>
@@ -49,7 +53,7 @@ const ProductDisplay = (props) => {
             <div>XXL</div>
           </div>
         </div>
-        <button>ADD TO CART</button>
+        <button onClick={() => addToCart(product.id)}>ADD TO CART</button>
         <p className="productdisplay-right-category">
           <span>Category :</span> Women, T-Shirt, Crop Top Women, T-Shirt, Crop
           Top
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,20 +1,35 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_product from "../Components/Assets/all_product";
-
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
 const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    const contextValue = {all_product}    
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log(cartItems);
+  };
 
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const contextValue = { all_product, cartItems, addToCart, removeFromCart };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
-
-
