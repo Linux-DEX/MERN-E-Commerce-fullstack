ba7b802552f94e0bc40e827a4d61ea7b99ef2ff5
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-09 11:17:20 +0530

    login page design

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id=""/>
+          <p>By Continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
