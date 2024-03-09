491811077e7852b0f0c7e7421e30998caf7f15ac
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-09 14:50:37 +0530

    Related Product components frontend

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -6,7 +6,7 @@ const Item = (props) => {
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-        <img src={props.image} alt="" />
+        <img onClick={window.scrollTo(0,0)} src={props.image} alt="" />
       </Link>
       <p>{props.name}</p>
       <div className="item-prices">
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrum/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -14,6 +15,7 @@ const Product = () => {
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
       <DescriptionBox />
+      <RelatedProducts />
     </div>
   );
 };
