ea9df6afd5bce7c6c9faf134eb02b09e882d0d02
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-11 22:50:51 +0530

    created api for uploading product

--- backend/index.js
+++ backend/index.js
@@ -11,42 +11,109 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection with MongoDB
-mongoose.connect("mongodb+srv://sarabjeet:iamsarabjeet@cluster0.eg0zqws.mongodb.net/e-commerce");
+mongoose.connect(
+  "mongodb+srv://sarabjeet:iamsarabjeet@cluster0.eg0zqws.mongodb.net/e-commerce"
+);
 
 // API Creation
 
 app.get("/", (req, res) => {
-    res.send("Express App is Running");
+  res.send("Express App is Running");
 });
 
 // Image Storage Engine
 
 const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename: ( req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-
-const upload = multer({
-    storage: storage
-})
-
-// Creating Upload Endpoint for images
-app.use('/images', express.static('upload/images'));
-
-app.post("/upload", upload.single('product'), (req, res) => {
-    res.json({
-        success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
-    })
-})
-
-app.listen(port, (error) => {
-    if (!error) {
-        console.log(`Server running on port ${port}`);
-    } else {
-        console.log("Error: " +error);
-    }
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
 });
 
+const upload = multer({
+  storage: storage,
+});
+
+// Creating Upload Endpoint for images
+app.use("/images", express.static("upload/images"));
+
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+// Scheme for Creating products
+
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  console.log(product);
+  await product.save();
+  console.log("Saved");
+  res.json({ success: true, name: req.body.name });
+});
+
+// Creating API for Deleting Products
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log(`Server running on port ${port}`);
+  } else {
+    console.log("Error: " + error);
+  }
+});
