2080db464ac590801fe8611cd7cf4cf06874040e
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-12 11:04:14 +0530

    admin addProduct working succesfully

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -46,8 +46,15 @@ const AddProduct = () => {
     if (responseData.success) {
       product.image = responseData.image_url;
       console.log(product);
-      await fetch("http://localhost:4000/product", {
-        
+      await fetch("http://localhost:4000/addproduct", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      }).then((resp) => resp.json()).then((data) => {
+        data.success?alert("Product Added Successfully"):alert("Something went wrong");
       })
     }
   };
