e04e14a068f8b6d3ef229bfe507977358aa7d8b8
Linux-DEX <sarabjeet.9353@gmail.com>
2024-03-12 12:51:48 +0530

    successfully working of admin adding, remove and display all product

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,11 +1,79 @@
-import './ListProduct.css';
+import { useEffect, useState } from "react";
+import "./ListProduct.css";
+import cross_icon from "../../assets/cross_icon.png";
 
 const ListProduct = () => {
-  return (
-      <div className="list-product">
-          product
-    </div>
-  )
-}
+  const [allproducts, setAllProducts] = useState([]);
 
-export default ListProduct
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const remove_product = async (id) => {
+    await fetch("http://localhost:4000/removeproduct", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id:id
+      }),
+    })
+    await fetchInfo();
+  }
+
+  return (
+    <div className="list-product">
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className="listproduct-format-main listproduct-format"
+              >
+                <img
+                  src={product.image}
+                  alt=""
+                  className="listproduct-product-icon"
+                />
+                <p>{product.name}</p>
+                <p>&#8377;{product.old_price}</p>
+                <p>&#8377;{product.new_price}</p>
+                <p>{product.category}</p>
+                <img
+                  onClick={() => { remove_product(product.id) }}
+                  className="listproduct-remove-icon"
+                  src={cross_icon}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ListProduct;
